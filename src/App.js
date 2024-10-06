2770f20c19398347eb84e766c2efe25022b3cfa2
nzhong12 <NathanYZhong@gmail.com>
2024-10-06 13:07:20 -0400

    added sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './colleges.json';
 import Table from 'react-bootstrap/Table';
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, useSubmit, } from "react-router-dom";
 
 const formatDollar = ( (x) => 
   x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
@@ -23,31 +24,44 @@ function App() {
   .then((json) => console.log(json));  */
 
   return (
-    <div className="App">
-      <Button variant="primary">Top 300 National Universities</Button>{' '}
-      {/* <Button variant="secondary">Secondary</Button>{' '} */}
-      <br />
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>School</th>
-            <th>Tuition</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-            data.map((item, i) => 
-              <tr key={i}>
-                <td>{item.displayRank}</td>
-                <td>{item.displayName}</td>
-                <td>${item.tuition? formatDollar(item.tuition) : ""}</td>
-              </tr>
-            )
-        }
-        </tbody>
-      </Table>
-    </div>
+    <>
+       <div id="sidebar">
+          <h1>Search</h1>
+          <div>
+          <nav>TODO
+          </nav>
+          <Button variant="primary">Submit</Button>  
+          </div>
+          
+        </div>
+      <div id="detail">
+        {/* <div><Button variant="primary">Top 300 National Universities</Button></div> */}
+        <h1>College List</h1>
+        <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>School</th>
+              <th>Tuition</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+              data.map((item, i) => 
+                <tr key={i}>
+                  <td>{item.displayRank}</td>
+                  <td>{item.displayName}</td>
+                  <td>${item.tuition? formatDollar(item.tuition) : ""}</td>
+                </tr>
+              )
+          }
+          </tbody>
+        </Table>
+        </div>
+      </div> 
+    </>
+   
   );
 }
 
