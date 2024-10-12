43f8c289a58deff8979daf70a774fe0a47a5dd02
nzhong12 <NathanYZhong@gmail.com>
2024-10-12 00:49:05 -0400

    dynamic icon, mouse zoom and max zoom completed

--- src/MyMap.jsx
+++ src/MyMap.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 import data from './colleges.json';
 import L from 'leaflet';
 
+const SetMapBounds = () => {
+  const map = useMap(); // Access the map instance
+  
+  useEffect(() => {
+    // Create an array of lat/lng coordinates
+    const southwest = [35, -124.4194]; // San Francisco
+    const northeast = [41, -70.0060];  // New York
+    const southwest1 = [31, -128.4194]; 
+    const northeast1 = [45, -68.0060]; 
+    // Fit the map to these bounds
+    map.fitBounds([southwest, northeast], { padding: [100, 100] });
+    map.setMaxBounds([southwest1, northeast1]);
+    //map.setMaxBoundsViscosity(0.8);
+
+    map.setMinZoom(4);
+    map.setMaxZoom(10);
+  }, [map]);
+
+  return null;
+};
+
 const Map = () => {
     // The center of the map, you can change the initial position if needed
-    const initialPosition = [42.373611, -71.109733]; // Example: Harvard
+    const initialPosition = [38, -97]; // Example: Harvard
     const colleges = data.slice(0,20);
     console.log(colleges.length);
 
@@ -20,20 +41,31 @@ const Map = () => {
       shadowSize: [41, 41]
     });
 
+    const brownIcon = new L.Icon({
+      iconUrl: './brown.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 25],
+      //iconAnchor: [12, 41],
+      //popupAnchor: [1, -34],
+      shadowSize: [35, 35]
+    });
+
     return (
       <div>
         <h2>College Map</h2>
       
-        <MapContainer center={initialPosition} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={initialPosition} zoom={10} scrollWheelZoom={true} maxBoundsViscosity={0.6}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {colleges.map((college, index) => (
-            <Marker icon={greenIcon} key={`item-${index}`} position={[Number(college.LAT), Number(college.LON)]}>
+            <Marker icon={index%2== 0? brownIcon : greenIcon} key={`item-${index}`} position={[Number(college.LAT), Number(college.LON)]}>
               <Popup>{college.displayName}</Popup>
             </Marker>
           ))}
+
+          <SetMapBounds />
         </MapContainer>
       </div>    
     );
